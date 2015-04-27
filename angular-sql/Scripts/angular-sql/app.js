d7279c5388ac01922ecc243b8e059bf9df95f393
SalsaAddict <pierre.henry@salsaaddict.com>
2015-04-27 13:47:58 +0100

    Second

---
+++ angular-sql/Scripts/angular-sql/app.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module("angular-sql", ["ngRoute", "ui.bootstrap"]);
+
+app.config(["$logProvider", "$routeProvider", function ($logProvider, $routeProvider) {
+    $logProvider.debugEnabled(true);
+    $routeProvider
+        .when("/home", { caseInsensitiveMatch: true, templateUrl: "views/home.html" })
+        .otherwise({ redirectTo: "/home" });
+}]);
